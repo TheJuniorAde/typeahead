cec8d9e8b80f904aba4255f25b2c4a3b8abb3597
TheJuniorAde <adeildo.mesquita@gmail.com>
2024-01-31 00:00:07 -0300

    Fine tuning styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react"
 import "./App.css"
-import { Typeahead, TypeaheadSelectedItemsList } from "./components/Typeahead"
+import {
+  Typeahead,
+  TypeaheadSelectedItem,
+  TypeaheadSelectedItemsList,
+} from "./components/Typeahead"
 
 function App() {
   const [, setValues] = useState<TypeaheadSelectedItemsList>([])
+  const [, setValue] = useState<TypeaheadSelectedItem>()
 
   return (
     <div className="App">
@@ -12,9 +17,22 @@ function App() {
         onChange={(newValues) => {
           setValues((newValues as TypeaheadSelectedItemsList) || [])
         }}
-        label="Typeahead"
-        placeholder="With Custom Placeholder"
+        label="Multiple"
+        placeholder="custom placeholder"
         selected={[{ id: 1, label: "Value" }]}
+        options={Array(1000)
+          .fill(null)
+          .map((_: null, index) => ({
+            id: index + 1,
+            label: `Value ${index + 1}`,
+          }))}
+      />
+      <Typeahead
+        onChange={(newValue) => {
+          setValue((newValue as TypeaheadSelectedItem) || null)
+        }}
+        label="Single"
+        placeholder="custom placeholder"
         options={Array(1000)
           .fill(null)
           .map((_: null, index) => ({
